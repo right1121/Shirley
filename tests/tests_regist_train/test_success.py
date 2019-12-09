d3036eb9fdbba436bc2aa13ff44dfa26ea7b69a0
MsakiHoshino <right1121@hotmail.co.jp>
2019-12-10 00:05:41 +0900

    正常系テスト追加

--- tests/tests_regist_train/test_success.py
+++ tests/tests_regist_train/test_success.py
@@ -1,6 +1,28 @@
+import pytest
+
 import lambda_function
 
 
-def test_success():
-    res = lambda_function.main()
-    assert res["statusCode"] == 200
+@pytest.fixture
+def regist_data():
+    return {
+        "company": "JR東",
+        "maker": "KATO",
+        "series": "E231",
+        "cars": 10
+    }
+
+
+def test_response_setting(regist_data):
+    body = regist_data
+    res = lambda_function.main(body)
+    assert res['statusCode'] == 200
+    assert res['headers']['Content-Type'] == 'application/json; charset=utf-8'
+    assert res['headers']['Access-Control-Allow-Origin'] == '*'
+
+
+def test_insert_data(regist_data):
+    body = regist_data
+    res = lambda_function.main(body)
+
+    assert res['statusCode'] == 200
