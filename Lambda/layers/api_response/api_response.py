076059396417c23ec2992d4e41838a9ae38fc09d
MsakiHoshino <right1121@hotmail.co.jp>
2019-12-11 23:38:10 +0900

    エラー用のレスポンス追加

--- Lambda/layers/api_response/api_response.py
+++ Lambda/layers/api_response/api_response.py
@@ -1,13 +1,16 @@
 import json
+from http import HTTPStatus
 
 
 class api_response():
-    def __init__(self):
-        self.__status_code = 200
-        self.__headers = {
+    __headers = {
             'Content-Type': 'application/json; charset=utf-8',
             'Access-Control-Allow-Origin': '*'
         }
+
+    def __init__(self):
+        self.__status_code = HTTPStatus.OK
+        self.__headers = api_response.__headers
         self.__body = {}
 
     @property
@@ -34,9 +37,35 @@ class api_response():
     def body(self, _body):
         self.__body = _body
 
-    def format_response(self):
+    def format(self):
         return {
             'statusCode': self.__status_code,
             'headers': self.__headers,
             'body': json.dumps(self.__body)
+        }
+
+    @classmethod
+    def validation_error(cls, body=None):
+        if body is None:
+            body = {}
+        elif type(body) is not dict:
+            raise TypeError
+
+        return {
+            'statusCode': HTTPStatus.BAD_REQUEST,
+            'headers': api_response.__headers,
+            'body': json.dumps(body)
+        }
+
+    @classmethod
+    def exception_error(cls, body=None):
+        if body is None:
+            body = {}
+        elif type(body) is not dict:
+            body = {}
+
+        return {
+            'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR,
+            'headers': api_response.__headers,
+            'body': json.dumps(body)
         }
