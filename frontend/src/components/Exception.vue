f0f7d7d39ccd8cd2906da918c1d724561094483c
MsakiHoshino <right1121@hotmail.co.jp>
2019-12-18 01:31:14 +0900

    500エラー時の遷移先作成

---
+++ frontend/src/components/Exception.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>サーバーで異常が発生しました。</h1>
+    </div>
+</template>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import TrainNew from '../components/TrainNew.vue'
+import Exception from '../components/Exception.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/train/new',
     name: 'TrainNew',
     component: TrainNew
+  },
+  {
+    path: '/exception',
+    name: 'Exception',
+    component: Exception
   }
 ]
 
