cbc4ac4d80d5d04bb05d248a2a70dc145bd087d9
MsakiHoshino <right1121@hotmail.co.jp>
2019-12-31 16:54:57 +0900

    beforeEachをbeforeResolveに変更
    amplifyのサンプルがbeforeResolveを使用していたため

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -37,7 +37,7 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach(async (to, from, next) => {
+router.beforeResolve(async (to, from, next) => {
   if (to.matched.some(record => record.meta.isPublic)) {
     return next()
   }
