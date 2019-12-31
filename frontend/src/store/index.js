02d4d28fdefb10c7c25ad053cb70c082b71c1f59
MsakiHoshino <right1121@hotmail.co.jp>
2019-12-31 22:55:49 +0900

    userのgetterを追加

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -7,6 +7,11 @@ export default new Vuex.Store({
   state: {
     user: null
   },
+  getters: {
+    getUser: state => {
+      return state.user
+    }
+  },
   mutations: {
     setUser(state, user) {
       state.user = user
