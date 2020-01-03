ab961be603b28f790639e7bfb0dacf3588c2da8d
MsakiHoshino <right1121@hotmail.co.jp>
2020-01-04 01:57:56 +0900

    ダイアログのテンプレート作成

--- frontend/src/views/Train.vue
+++ frontend/src/views/Train.vue
@@ -14,9 +14,20 @@
 
           <v-spacer></v-spacer>
 
-          <v-toolbar-items>
-            <v-btn text>Link 1</v-btn>
-          </v-toolbar-items>
+          <v-dialog v-model="dialog" max-width="600px">
+            <template v-slot:activator="{ on }">
+              <v-btn color="primary" dark v-on="on">車両登録</v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">車両登録</span>
+              </v-card-title>
+              <v-card-text>
+              </v-card-text>
+              <v-card-actions>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-toolbar>
       </template>
     </v-data-table>
@@ -29,6 +40,7 @@ import { Auth } from 'aws-amplify'
   export default {
     data () {
       return {
+        dialog: false,
         loading: true,
         headers: [
           {
