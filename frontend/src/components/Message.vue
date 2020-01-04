61b4d0ff941f5f95280ad4add317a74885e05e9e
MsakiHoshino <right1121@hotmail.co.jp>
2020-01-04 16:11:04 +0900

    snackbar追加

---
+++ frontend/src/components/Message.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-snackbar
+    v-model="snackbar"
+  >
+    {{ text }}
+  </v-snackbar>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        snackbar: false,
+        text: 'Hello, I\'m a snackbar',
+      }
+    },
+  }
+  </script>
