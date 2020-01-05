291d7bdf53bfa4b297146d7da961775b67811ca2
MsakiHoshino <right1121@hotmail.co.jp>
2020-01-06 01:17:49 +0900

    LPページ作成

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,64 @@
 <template>
   <div class="home">
-    <amplify-authenticator v-bind:authConfig="authConfig"></amplify-authenticator>
+    <v-toolbar color="white">
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <section>
+        <v-parallax src="@/assets/hero.jpg" height="600">
+          <v-layout
+            column
+            align-center
+            justify-center
+            class="white--text"
+          >
+            <h1 class="white--text mb-2 display-1 text-center">大切な車両の管理サイト</h1>
+            <div class="subheading mb-4 text-center">鉄道模型管理サイト『Shirley』</div>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-layout
+          column
+          wrap
+          class="my-12"
+          align-center
+        >
+          <v-flex xs12 sm4 class="my-4">
+            <div class="text-center">
+              <span class="subheading">
+                Service
+              </span>
+              <h2 class="headline">機能</h2>
+            </div>
+          </v-flex>
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+                <v-flex xs12 md12>
+                  <v-card flat class="transparent">
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-center">保有車両確認</div>
+                    </v-card-title>
+                    <v-card-text>
+                      保有している車両を一覧で表示。自分がどのような車両を保有しているのか確認できます。
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </section>
+      <section>
+        <amplify-authenticator v-bind:authConfig="authConfig"></amplify-authenticator>
+      </section>
+      <v-footer color="blue darken-2">
+      </v-footer>
+    </v-content>
   </div>
+  
 </template>
 
 <script>
@@ -11,6 +68,7 @@ export default {
   name: 'home',
   data() {
     return {
+      title: 'Shirley',
       authConfig: {
         signUpConfig: {
           usernameAttributes: 'ユーザー名',
