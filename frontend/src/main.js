052ccacb1a4a6f967b59c6f597836f4d205cd407
MsakiHoshino <right1121@hotmail.co.jp>
2020-01-07 22:55:33 +0900

    VueAdsenseのスクリプト埋め込み

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,12 @@ import api from './api'
 import AmplifyConfig from './amplify'
 import Amplify, * as AmplifyModules from 'aws-amplify'
 import { AmplifyPlugin } from 'aws-amplify-vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import VueAdsense from 'vue-adsense'
 
 Amplify.configure(AmplifyConfig)
+
+Vue.component('adsense', VueAdsense)
 
 Vue.use(AmplifyPlugin, AmplifyModules)
 
